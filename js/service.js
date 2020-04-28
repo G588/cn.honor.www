aa093731a927b2ab9ca007f29006a6458f1827ba
G588 <1466905783@qq.com>
2020-04-28 22:08:32 +0800

    更改

--- js/service.js
+++ js/service.js
@@ -1,3 +1,20 @@
-$(function () {//0
+$(function () { //0
 
-})//0
+    //查看更多-切换为-收起
+    $(".v3-s-cn-more a ").eq(0).on('click', function () {
+        $(".v3-s-cn-more a ").eq(0).hide()
+        $(".v3-s-cn-more a ").eq(1).show()
+        // 更多商品显示
+        $(".v3-hidden").show()
+    })
+    //收起-切换为-查看更多
+    $(".v3-s-cn-more a ").eq(1).on('click', function () {
+        $(".v3-s-cn-more a ").eq(0).show()
+        $(".v3-s-cn-more a ").eq(1).hide()
+        // 更多商品隐藏
+        $(".v3-hidden").hide()
+    })
+
+
+
+}) //0
