1735448cfd724d6b8a5d9da3189db8d464e03182
G588 <1466905783@qq.com>
2020-04-30 09:00:30 +0800

    更改

--- js/service.js
+++ js/service.js
@@ -7,6 +7,7 @@ $(function () { //0
         // 更多商品显示
         $(".v3-hidden").show()
     })
+
     //收起-切换为-查看更多
     $(".v3-s-cn-more a ").eq(1).on('click', function () {
         $(".v3-s-cn-more a ").eq(0).show()
@@ -15,6 +16,19 @@ $(function () { //0
         $(".v3-hidden").hide()
     })
 
+    //鼠标点击，显示边框和小三角,再次点击隐藏边框和小三角
+    //当前的显示边框和小三角，他的兄弟隐藏边框和小三角
+    $(".swiper-slide").on('click', function () {
+        $(this).toggleClass('sd').siblings().removeClass('sd')
+        $(this).toggleClass('pox').siblings().removeClass('pox')
+    })
+
+$(".v3-s-cn-type-content-type-item ").on('mouseenter',function(){
+    $(".v3s").toggle()
+})
+
+
 
 
 }) //0
+
