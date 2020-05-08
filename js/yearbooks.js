feba9fd5ab393d3b08ae0b4bd39fe91253c373a1
G588 <1466905783@qq.com>
2020-05-08 23:14:20 +0800

    更改

--- js/yearbooks.js
+++ js/yearbooks.js
@@ -5,13 +5,25 @@ $(function () { //0
         index = $(this).index()
         $('.gy-share-black').eq(index).hide().next().show()
     })
+    
     //鼠标移出显示暗的新浪与QQ空间图片
     $(".gy-share-list li").on('mouseout', function () {
         $('.gy-share-black').eq(index).show().next().hide()
     })
 
-$(".swiper-wrapper").scroll(function(){
-    console.log($(".swiper-wrapper").scrollLeft());
-})
-    // 二
+    $(".swiper-wrapper").scroll(function () {
+        console.log($(".swiper-wrapper").scrollLeft());
+    })
+
+    // 鼠标移入，显示遮罩
+    $('.swiper-slide').on('mouseover', function () {
+        var index = $(this).index()
+        $('.gy-more-author').hide()
+        $('.gy-more-author').eq(index).show()
+    })
+
+    $('.swiper-slide').on('mouseout', function () {
+        $('.gy-more-author').hide()
+    })
+
 }) //0
